436cd7ddea4947af45ef198cda1fe8f584c69d3c
kkig <kigoshi.kanazawa@gmail.com>
2019-12-23 11:41:16 +0100

    init commit

---
+++ src/components/AppHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AppHeader.css';
+
+const AppHeader = () => {
+    return (
+        <div className="app-title-texts">
+            <h1>Pokedex</h1>
+            <small>Find out detail of your favourite Pokemon!</small>
+        </div>
+    );
+}
+
+export default AppHeader;
