b17341c07669ca1a651d9efdd5888d7b603e6bdb
kkig <kigoshi.kanazawa@gmail.com>
2020-01-11 22:47:26 +0100

    evolution text adjust

--- src/components/AppBody/PokeList.js
+++ src/components/AppBody/PokeList.js
@@ -124,7 +124,6 @@ const PokeList = ({ listData }) => {
                     } while (!!evoData && evoData.hasOwnProperty('evolves_to'));
 
                     const evolInfo = { evolURL: evolChainURL, evolutions: evoChain };
-                    console.log(evolInfo);
 
                     // Update data on mobX
                     store.addEvolChain(evolInfo);
